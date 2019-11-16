ad3d17dbdd6ada51816c141b01fd8d7582d79a0b
brunorech <brunogrech@hotmail.com>
2019-11-16 15:00:37 -0300

    Fixing server response function issues

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Routes from './src/Routes';
-import Login from './src/Pages/Login';
-import SignUp from './src/Pages/SignUp';
 
 export default () => {
   return (
--- src/Connection/Server.js
+++ src/Connection/Server.js
@@ -2,28 +2,28 @@ import TcpSocket from 'react-native-tcp-socket';
 import BackgroundTimer from 'react-native-background-timer';
 import { SUCCESS_MESSAGE, ERROR_MESSAGE, DATA_MESSAGE, CONNECTED_STATUS_MESSAGE } from './MessageTypes';
 
+let dataFunction;
 const Server = TcpSocket.createConnection({
-  host: '192.168.0.100',
+  host: '192.168.0.101',
   //  host: '192.168.2.151',
   port: 56000,
   interface: 'wifi',
 });
 
+if (!dataFunction) {
+  dataFunction = data => { console.log(data.toString('utf8')) };
+  Server.on('data', data => dataFunction(data.toString('utf8')));
+}
+
 BackgroundTimer.runBackgroundTimer(() => {
-  sendData(CONNECTED_STATUS_MESSAGE, '', null, error => console.log('erro: ',error));
+  sendData(CONNECTED_STATUS_MESSAGE, '', null, error => console.log('erro: ', error));
 },
   5000);
 
 export const sendData = async (messageType = '', message, onSuccess, onError) => {
 
-  if (typeof message !== 'string') {
-    await Server.write(messageType + JSON.stringify(message) + "\n");
-  } else {
-    await Server.write(messageType + message + "\n");
-  }
-
-  await Server.on('data', data => {
-    const message = data.toString('utf8').match("([A-Z]+>?)(.*)");
+  dataFunction = response => {
+    const message = response.match("([A-Z]+>?)(.*)");
     const status = message[1];
     const messageContent = message[2].trim().length > 0 ? JSON.parse(message[2]) : '';
 
@@ -32,5 +32,12 @@ export const sendData = async (messageType = '', message, onSuccess, onError) =>
     } else if (onSuccess && (status === SUCCESS_MESSAGE || status === DATA_MESSAGE)) {
       return onSuccess(messageContent);
     }
-  });
+  };
+
+  if (typeof message !== 'string') {
+    await Server.write(messageType + JSON.stringify(message) + "\n");
+  } else {
+    await Server.write(messageType + message + "\n");
+  }
+
 }
